7cf92542ba6ea01aea064e8de9f700e7ad11590c
mjuned <mmj.nowaz@gmail.com>
2023-02-27 00:03:54 +0000

    Add header and add restaurant fields to home page

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Header from "../components/Header"
+import Header from "../components/Header";
+import AddRestaurant from "../components/AddRestaurant";
+import RestaurantList from "../components/RestaurantList";
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <AddRestaurant/>
     </div>
   );
 };
