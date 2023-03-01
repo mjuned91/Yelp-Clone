4d56f7ded16f377ff574ae5d807fbf9c0ea98597
mjuned <mmj.nowaz@gmail.com>
2023-03-01 23:06:15 +0000

    Add states for user input fields

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateRestaurant = (props) => {
   const {id} = useParams();
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("");
   
   return (
     <div>
       <form action="">
         <div class="form-group">
           <label htmlFor="name">Name</label>
-          <input id="name" class="form-control" type="text"></input>
+          <input value={name} onChange={e => setName(e.target.value)} id="name" class="form-control" type="text"></input>
         </div>
 
         <div class="form-group">
           <label htmlFor="location">Location</label>
-          <input id="location" class="form-control" type="text" />
+          <input value={location} onChange={e => setLocation(e.target.value)} id="location" class="form-control" type="text" />
         </div>
 
         <div class="form-group">
           <label htmlFor="price_range">Price Range</label>
-          <input id="price_range" class="form-control" type="number" />
+          <input value={priceRange} onChange={e => setPriceRange(e.target.value)} id="price_range" class="form-control" type="number" />
         </div>
         <button class="btn btn-primary">Submit</button>
       </form>
