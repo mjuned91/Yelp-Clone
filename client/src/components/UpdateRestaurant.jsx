9b3712e08032a7ed5990ee48c6b171cac7d2d3ad
mjuned <mmj.nowaz@gmail.com>
2023-03-01 22:43:11 +0000

    Add form for user input on update page

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -6,7 +6,23 @@ const UpdateRestaurant = (props) => {
   
   return (
     <div>
+      <form action="">
+        <div class="form-group">
+          <label htmlFor="name">Name</label>
+          <input id="name" class="form-control" type="text"></input>
+        </div>
 
+        <div class="form-group">
+          <label htmlFor="location">Location</label>
+          <input id="location" class="form-control" type="text" />
+        </div>
+
+        <div class="form-group">
+          <label htmlFor="price_range">Price Range</label>
+          <input id="price_range" class="form-control" type="number" />
+        </div>
+        <button class="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 };
