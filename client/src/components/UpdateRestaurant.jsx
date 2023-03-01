e6f26940fa33ffbec1a9cef41d83a1ce72fe99cb
mjuned <mmj.nowaz@gmail.com>
2023-03-01 23:26:55 +0000

    Updates added to Restaurants table and redirected to home page

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const UpdateRestaurant = (props) => {
+  const navigate = useNavigate();
   const {id} = useParams();
+  const { restaurants } = useContext(RestaurantsContext);
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
   const [priceRange, setPriceRange] = useState("");
   
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await RestaurantFinder.get(`/${id}`);
+      setName(response.data.data.restaurant.name);
+      setLocation(response.data.data.restaurant.location);
+      setPriceRange(response.data.data.restaurant.price_range);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updateRestaurant = await RestaurantFinder.put(`/${id}`, {
+      name,
+      location,
+      price_range: priceRange
+    });
+    navigate("/");
+  };
+
   return (
     <div>
       <form action="">
@@ -24,7 +49,7 @@ const UpdateRestaurant = (props) => {
           <label htmlFor="price_range">Price Range</label>
           <input value={priceRange} onChange={e => setPriceRange(e.target.value)} id="price_range" class="form-control" type="number" />
         </div>
-        <button class="btn btn-primary">Submit</button>
+        <button type="submit" onClick={handleSubmit} class="btn btn-primary">Submit</button>
       </form>
     </div>
   );
