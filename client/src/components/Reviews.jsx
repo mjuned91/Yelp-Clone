eac93e2e103235e001c12d11c6852047c7a2e903
mjuned <mmj.nowaz@gmail.com>
2023-03-10 17:15:02 +0000

    Add key to Reviews.jsx to get rid of error

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import StarRating from './StarRating';
 
 const Reviews = ( {reviews} ) => {
-  console.log(reviews);
   return (
     <div class="row row-cols-3 mb-2">
       {reviews.map((review) => {
         return (
-          <div class="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+          <div key={review.id} class="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
             <div class="card-header d-flex justify-content-between">
               <span>{review.name}</span>
               <span><StarRating rating={review.rating}/></span>
