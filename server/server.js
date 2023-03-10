f2d9dde291d6d5296a14a716c556c658d493a000
mjuned <mmj.nowaz@gmail.com>
2023-03-10 22:52:00 +0000

    Add express route for storing new review into db

--- server/server.js
+++ server/server.js
@@ -93,6 +93,22 @@ app.delete("/api/v1/restaurants/:id", async (req, res) => {
   }
 });
 
+//Store review in db
+app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
+  try {
+    const newReview = await db.query("INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) RETURNING *", [req.params.id, req.body.name, req.body.review, req.body.rating]);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        review: newReview.rows[0]
+      }
+    });
+  } catch (err) {
+      console.log(err);
+  };
+});
+
 app.listen(port, () => {
   console.log(`Server is up and listening on port ${port}`);
 });
